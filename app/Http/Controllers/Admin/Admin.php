58f25d83db30c3c44cb2ad8f339f4772208828bf
znq <3010817497@qq.com>
2018-09-25 10:53:46 +0800

    znq

--- app/Http/Controllers/Admin/Admin.php
+++ app/Http/Controllers/Admin/Admin.php
@@ -7,5 +7,33 @@ use App\Http\Controllers\Controller;
 
 class Admin extends Controller
 {
-    //
+    /** 管理员添加 */
+    public function admin_add(){
+
+    }
+
+    /** 执行添加 */
+    public function admin_add_do(){
+
+    }
+
+    /** 管理员列表 */
+    public function admin_list(){
+
+    }
+
+    /** 管理员修改 */
+    public function admin_modify(){
+
+    }
+
+    /** 执行修改 */
+    public function admin_modify_do(){
+
+    }
+
+    /** 管理员删除 */
+    public function admin_delete(){
+
+    }
 }
--- app/Http/Controllers/Admin/Type.php
+++ app/Http/Controllers/Admin/Type.php
@@ -7,5 +7,45 @@ use App\Http\Controllers\Controller;
 
 class Type extends Controller
 {
-    //
+    /***
+     * 类型添加
+     */
+    public function type_add(){
+
+    }
+
+    /**
+     * 执行添加
+     */
+    public function type_add_do(){
+
+    }
+
+    /**
+     *类型列表
+     */
+    public function type_list(){
+
+    }
+    /**
+     * 类型修改
+     */
+    public function type_modify(){
+
+    }
+
+    /**
+     * 执行修改
+     */
+    public function type_modify_do(){
+
+    }
+
+    /**
+     * 类型删除
+     */
+    public function type_delete(){
+
+    }
+
 }
