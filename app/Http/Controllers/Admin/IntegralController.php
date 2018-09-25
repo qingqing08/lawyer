da9df65c63149bfcd183e82002805aa625315863
wangbadan <626979542@qq.com>
2018-09-25 09:28:04 +0800

    mz-控制器

---
+++ app/Http/Controllers/Admin/IntegralController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IntegralController extends Controller
+{
+    /**
+     * 积分详情列表
+     */
+    public function integral_list(){
+        
+    }
+}
---
+++ app/Http/Controllers/Admin/LawyerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LawyerController extends Controller
+{
+    /**
+     *  用户/律师列表
+     */
+    public function lawyer_list(){
+
+    }
+
+    /**
+     * 执行删除用户/律师操作
+     */
+    public function lawyer_delete(){
+        
+    }
+}
