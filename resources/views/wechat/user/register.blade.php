293cfe11cf3a6a1123a122c2c5c3f8445deb1a5d
zhuang <626979542@qq.com>
2018-09-27 09:14:58 +0800

    个人中心

--- resources/views/wechat/user/register.blade.php
+++ resources/views/wechat/user/register.blade.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>完善信息</title>
-    <meta name="renderer" content="webkit">
+    <meta name="renderer" content="webkit|ie-comp|ie-stand">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" href="/lib/layui/css/layui.css"  media="all">
-    <!-- 注意：如果你直接复制所有代码到本地，上述css路径需要改成你本地的 -->
+    <meta name="viewport" content="width=device-width,user-scalable=yes, minimum-scale=0.4, initial-scale=0.8,target-densitydpi=low-dpi" />
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="./layui/css/font.css">
+    <link rel="stylesheet" href="./layui/css/xadmin.css">
+    <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script src="./layui/lib/layui/layui.js" charset="utf-8"></script>
+    <script type="text/javascript" src="./layui/js/xadmin.js"></script>
 </head>
 <body>
-<form class="layui-form" action="" lay-filter="example">
-    <div class="layui-form-item">
-        <label class="layui-form-label">单选框</label>
-        <div class="layui-input-block">
-            <input type="radio" name="sex" value="男" title="男" checked="">
-            <input type="radio" name="sex" value="女" title="女">
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-input-block">
-            <button class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>
-        </div>
-    </div>
-</form>
+<from>
+    请选择
+    <input type="radio" name="register" value="普通">普通
+    <input type="radio" name="register" value="律师">律师
+</from>
