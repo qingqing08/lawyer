14d69154caeec0a465662875c7fb17ea29efe31d
Dengdeng <chilegezhu@163.com>
2018-09-27 11:21:50 +0800

    Dengdeng

--- resources/views/wechat/hotspot/hotspot_view.blade.php
+++ resources/views/wechat/hotspot/hotspot_view.blade.php
@@ -20,8 +20,12 @@
 </div>
     
     <div class="new_ask" >
-        <span> <?php echo date('M-d H:i:s',$datainfo->h_ctime);?> </span>
+        <span> <?php echo date('m-d H:i:s',$datainfo->h_ctime);?> </span>
+        <br>
             <p>{{@$datainfo->h_content}}</p>              
+    </div>
+    <div>
+        <a id="pin">评论</a>
     </div>
     <br>
     <br>
@@ -46,7 +50,12 @@
     </footer>
 </body>
 </html>
+<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript">
+
+    $("#pin").on('click',function(){
+        alert('nihao');
+    });
 
 $(function(){
     $(window).bind("scroll",function(){
