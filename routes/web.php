1c3585fba0cedfd76d99330542d77256f79a16f1
zhuang <626979542@qq.com>
2018-09-28 15:14:00 +0800

    tixian

--- routes/web.php
+++ routes/web.php
@@ -138,6 +138,15 @@ Route::get('/selfShow' , 'Wechat\User@selfShow');
 Route::get('/Code' , 'Wechat\User@Code');
 Route::get('/generateCode' , 'Wechat\User@generateCode');
 
+/* 提现*/
+Route::get('/forward' , 'Wechat\User@forward');
+/* 提现到微信*/
+Route::get('/wechat' , 'Wechat\User@wechat');
+/* 提现到银行卡*/
+Route::get('/bankCard' , 'Wechat\User@bankCard');
+/* 提现到支付宝*/
+Route::get('/alipay' , 'Wechat\User@alipay');
+
 /* 法律常识 */
 Route::get('/knowledge-list' , 'Wechat\Knowledge@knowledge_list');
 //常识列表
