a5601e4d403f973eaa7064332a38a3ee453537e9
zhuang <626979542@qq.com>
2018-09-28 15:03:12 +0800

    tixian

--- app/Http/Controllers/Wechat/User.php
+++ app/Http/Controllers/Wechat/User.php
@@ -189,7 +189,7 @@ class User extends Controller{
             'desc' => '提现',
             'spbill_create_ip' => $_SERVER['REMOTE_ADDR']
         ];
-
+        dd($params);
         //去除数组的空值
         array_filter($params);
         if(isset($params['sign'])){
@@ -224,26 +224,7 @@ class User extends Controller{
         }
 
         $xml = ArrToXml($params);
-
-        //post 发送数据
-        function curlRequest($url,$data = ''){
-            $ch = curl_init();
-            $params[CURLOPT_URL] = $url;    //请求url地址
-            $params[CURLOPT_HEADER] = false; //是否返回响应头信息
-            $params[CURLOPT_RETURNTRANSFER] = true; //是否将结果返回
-            $params[CURLOPT_FOLLOWLOCATION] = true; //是否重定向
-            $params[CURLOPT_TIMEOUT] = 30; //超时时间
-            if(!empty($data)){
-                $params[CURLOPT_POST] = true;
-                $params[CURLOPT_POSTFIELDS] = $data;
-            }
-            $params[CURLOPT_SSL_VERIFYPEER] = false;//请求https时设置,还有其他解决方案
-            $params[CURLOPT_SSL_VERIFYHOST] = false;//请求https时,其他方案查看其他博文
-            curl_setopt_array($ch, $params); //传入curl参数
-            $content = curl_exec($ch); //执行
-            curl_close($ch); //关闭连接
-            return $content;
-        }
+        
         //post 带证书
         function postData($url,$postfields){
 
